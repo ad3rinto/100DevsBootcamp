ff52e2dc641ec042f774b88eebbc1f5f74bb6d74
ad3rinto <aad3rinto@gmail.com>
2023-05-11 12:36:16 +0100

    fixed

--- Class14-Materials-2022/simple-functions/js/main.js
+++ Class14-Materials-2022/simple-functions/js/main.js
@@ -1,5 +1,7 @@
 //create a function that adds two numbers and alerts the sum
+addNumbers = (num1, num2) => alert(num1 + num2);
 
 //create a function that multiplys three numbers and console logs the product
-
+multi = (a, b, c) => console.log(a * b * c);
+multi(2, 4, 6)
 //create a function that divides two numbers and returns the ???
